7783eb9f1d830cb36be0e72657fc2221ec4dc2c2
husaker <bearandv@gmail.com>
2025-06-28 18:43:17 +0300

    fixed system to work on server

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,6 +1,9 @@
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+import os
+import tempfile
+import json
 
 def load_spotify_data_from_sheets(sheet_url, credentials_path):
     """
@@ -13,6 +16,17 @@ def load_spotify_data_from_sheets(sheet_url, credentials_path):
     Returns:
         pd.DataFrame: DataFrame containing the Spotify listening data
     """
+    # Check for credentials in environment variable
+    credentials_json = os.getenv('GOOGLE_CREDENTIALS_JSON')
+    if credentials_json:
+        # If the value is a JSON string, ensure it's properly formatted
+        if credentials_json.startswith('{'):
+            credentials_json = json.dumps(json.loads(credentials_json))
+        # Create a temporary file for the credentials
+        with tempfile.NamedTemporaryFile(mode='w+', suffix='.json', delete=False) as tmp:
+            tmp.write(credentials_json)
+            tmp.flush()
+            credentials_path = tmp.name
     # Define the scope
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
